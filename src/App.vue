46962c88b85d145b78a0718caf51e4d87f4b6759
Winga <andywinga@hotmail.be>
2024-09-10 15:44:45 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import Nav from "./components/nav.vue";
+</script>
+
+<template>
+	<div class="flex flex-col h-full">
+		<header>
+			<nav />
+		</header>
+	</div>
+	<div class="text-3xl text-lime-500">COucou</div>
+</template>
---
+++ src/components/nav.vue
@@ -0,0 +1,5 @@
+<template>
+	<nav class="w-full bg-gray-900 fixed top-0">
+		<div>Mon site</div>
+	</nav>
+</template>
